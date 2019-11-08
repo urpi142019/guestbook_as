96ed647dbca8dce52baef61ea90e06f797e292e6
urpi142019 <57529154+urpi142019@users.noreply.github.com>
2019-11-08 17:12:46 -0500

    Create prepare-documents.js

---
+++ prepare-documents.js
@@ -0,0 +1,14 @@
+function main(params) {
+  if (!params.name || !params.comment) {
+    return Promise.reject({error: 'no name or comment'});
+  }
+
+	return {
+    doc: {
+      createdAt: new Date(),
+	    name: params.name,
+	    email: params.email,
+	    comment: params.comment,
+	  }
+  };
+}
