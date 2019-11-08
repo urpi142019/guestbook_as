298f7b9ed4db4bed845bc20f328207cd6c1cabbe
urpi142019 <57529154+urpi142019@users.noreply.github.com>
2019-11-08 17:29:29 -0500

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,4 +1,4 @@
-const apiUrl = 'https://service.us.apiconnect.ibmcloud.com/gws/apigateway/api/CHANGEME/guestbook';
+const apiUrl = 'https://c22ecf0f.us-south.apiconnect.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
